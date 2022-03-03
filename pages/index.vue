9f8d72c1cf76063bccd59f13e42309e96718db4f
Toya <toyack1128@gmail.com>
2022-03-03 23:08:40 +0900

    20220303

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6" class="mt-7">
+    <v-col cols="12" sm="8" md="6" class="mt-7 px-5">
 
     <div 
     v-for="post in sortedPosts"
