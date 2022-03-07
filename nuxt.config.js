c3f0791440aa7d3d4c8ecdc7abe8434fd2bcafb1
Toya <toyack1128@gmail.com>
2022-03-07 23:41:24 +0900

    20220306

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,6 +62,16 @@ export default {
     '@nuxtjs/axios',
   ],
 
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS,
+    debug: false
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS
+    }
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
